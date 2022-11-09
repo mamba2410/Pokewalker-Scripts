e360423827458066ec2d7b96889845a108890f3a
Mamba2410 <callummcgregor54@gmail.com>
2022-11-09 12:53:27 +0000

    img_to_bin fix bugs

--- img_to_bin.py
+++ img_to_bin.py
@@ -3,14 +3,16 @@ from PIL import Image
 from txt_to_bin import *
 from img_to_txt import *
 from slice_image import *
+import numpy as np
 
 def img_to_bin(in_file: Image, width: int, height: int, frames: int) -> bytearray:
-    data = np.array(in_img.convert('L').tobytes())
-    n_bytes = len(data)
+    data = np.array(bytearray(in_file.convert('L').tobytes()), dtype='uint8')
+    #n_bytes = len(data)
+    n_bytes = in_file.width*in_file.height
 
-    out = np.zeros(n_bytes//4)
+    out = np.zeros(n_bytes//4, dtype='uint8')
 
-    data = (data>>6)    ## get top 2 bits
+    data = ((data>>6)&3)    ## get top 2 bits
     for i in range(n_bytes):
         row = i//width
         col = i%width
@@ -20,7 +22,7 @@ def img_to_bin(in_file: Image, width: int, height: int, frames: int) -> bytearra
 
         pw_index = 2*(bitrow*width + col)
 
-        out[pw_index+0] |= (data[i]&2) << (bitpos-1)
+        out[pw_index+0] |= ((data[i]&2)>>1) << bitpos
         out[pw_index+1] |= (data[i]&1) << (bitpos)
 
     out_bytes = bytearray(out)
