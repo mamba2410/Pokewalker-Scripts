317ff611f5f6cd3d539427cfb1acae875d8bf675
Mamba2410 <callummcgregor54@gmail.com>
2022-11-09 12:53:57 +0000

    update bin_to_img

--- bin_to_img.py
+++ bin_to_img.py
@@ -1,27 +1,34 @@
 import sys
 from PIL import Image
-    
-def bin_to_img(in_bin):
-    inverted = bytes([b ^ 0b11111111 for b in in_bin])
-    return Image.frombytes('1', (16, 768), inverted)
-    
-def unslice_image(in_img):
+
+"""
+Accepts PW-style bytes, returns correctly encoded Image
+"""
+def bin_to_img(in_bin, width=64, height=48, frames=2, inverted=False):
+    if inverted:
+        data = bytes([b ^ 0b11111111 for b in in_bin])
+    else:
+        data = bytes(in_bin)
+    img = Image.frombytes('1', (16, width*height*frames//8), data)
+    img = unslice_image(img, width=width, height=height, frames=frames)
+    return img
+
+def unslice_image(in_img: Image, width=64, height=48, frames=2):
     if in_img == None:
         return in_img
 
-    animated = True
     in_img = in_img.convert('L')
 
-    lchunks = [in_img.crop((0,in_img.height-((i+1)*64),8,in_img.height-(i*64))) for i in range(0, 12 if animated else 6)]
-    rchunks = [in_img.crop((8,in_img.height-((i+1)*64),16,in_img.height-(i*64))) for i in range(0, 12 if animated else 6)]
-    
-    out_img = Image.new('L', (48*2,64))
+    lchunks = [in_img.crop((0,in_img.height-((i+1)*width),8,in_img.height-(i*width))) for i in range(height*frames//8)]
+    rchunks = [in_img.crop((8,in_img.height-((i+1)*width),16,in_img.height-(i*width))) for i in range(height*frames//8)]
 
-    for i in range(0,12 if animated else 6):
-        out_img.paste(Image.blend(lchunks[i],rchunks[i],2/3), (i*8, 0))
-    
+    out_img = Image.new('L', (height*frames,width))
+
+    for i in range(height*frames//8):
+        out_img.paste(Image.blend(lchunks[i],rchunks[i],1/2), (i*8, 0))
+
     return out_img.transpose(Image.ROTATE_90)
-    
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Correct syntax: bin_to_img.py in-file")
